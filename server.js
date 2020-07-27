4d8cdf14eb286080aaf526c58d5cfe73fb8f7537
siddharth-upamanyu <siddharth.upamanyu@unthinkable.co>
2020-07-28 04:01:14 +0530

    making changes for post again

--- server.js
+++ server.js
@@ -1,25 +1,45 @@
-const express = require('express');
-// const bodyParser = require('body-parser')
-const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+// const express = require('express');
+// // const bodyParser = require('body-parser')
+// const path = require('path');
+// const app = express();
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+// app.get('/ping', function (req, res) {
+//  return res.send('pong');
+// });
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// app.post('/', function(request, response) {
+//     response.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// app.get("/get", (req, res, next) => {
+//     res.json({
+//         "version": process.env.VERSION
+//     });
+// });
+
+// app.listen(process.env.PORT || 8080);
+
+const port = process.env.PORT || 8080
+
+var express = require("express");
+var app = express();
+const path = require('path');
+app.use(express.json());
+
+app.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready On Server http://localhost:${port}`)
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.post('/', function(request, response) {
+app.post('/', function (request, response) {
     response.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-app.get("/get", (req, res, next) => {
-    res.json({
-        "version": process.env.VERSION
-    });
-});
-
-app.listen(process.env.PORT || 8080);
