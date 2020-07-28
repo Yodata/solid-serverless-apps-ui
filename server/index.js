061e9eaad4d34afe00b149c64ed4227eba4b8ca9
siddharth-upamanyu <siddharth.upamanyu@unthinkable.co>
2020-07-28 06:53:53 +0530

    modified server

--- server/index.js
+++ server/index.js
@@ -3,9 +3,16 @@ const express = require("express");
 const app = express(); // create express app
 
 app.use(express.static(path.join(__dirname, "..", "build")))
-app.use(express.static("public"))
+
+app.post('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, "..", 'build', 'index.html'));
+});
+
+app.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, "..", 'build', 'index.html'));
+});
 
 // start express server on port 5000
 app.listen(5000, () => {
-  console.log("server started on port 5000");
+    console.log("server started on port 5000");
 });
