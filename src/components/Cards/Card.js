3184bcb3b5b8fcf40d586b5168e6f5f3ef46d9e4
siddharth-upamanyu <siddharth.upamanyu@unthinkable.co>
2020-08-12 18:55:34 +0530

    UI changes for card

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -33,8 +33,8 @@ const styles = theme => ({
     position: 'relative',
     backgroundColor: theme.palette.invisible.main,
     opacity: props => (props.application.connected ? 0.5 : !props.application.isVisible && 0.5),
-    minWidth: '29.3vw',
-    maxWidth: '29.3vw',
+    minWidth: '30vw',
+    maxWidth: '30vw',
     borderRadius: 0,
     boxShadow: '3px 3px 3px 2px rgba(0,0,0,0.3)'
   },
