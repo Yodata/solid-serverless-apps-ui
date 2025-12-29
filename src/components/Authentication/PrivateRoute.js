c0fdd8a638962ddfb007fe51a63dc12017ece4e7
adam-venord <adamvenord@hsfranchise.com>
2025-12-29 13:55:20 -0600

    fixed private route

--- src/components/Authentication/PrivateRoute.js
+++ src/components/Authentication/PrivateRoute.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import { Route, useLocation } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { currentUser } from '../../redux/actions/authenticationActions';
+import React from "react";
+import { Route, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { currentUser } from "../../redux/actions/authenticationActions";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-    const dispatch = useDispatch();
-    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-    let location = useLocation();
-    console.log("PrivateRoute rendering - isLoggedIn:", isLoggedIn, "location",location);
-    const role = location.search?.split("=")[1]
-    console.log("PrivateRoute role from URL:", role);
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  let location = useLocation();
+  console.log(
+    "PrivateRoute rendering - isLoggedIn:",
+    isLoggedIn,
+    "location",
+    location
+  );
+  let role = null;
+  if (location.pathname.includes("/agent")) role = "self";
+  else if (location.pathname.includes("/team")) role = "team";
+  console.log("PrivateRoute role from URL:", role);
 
-    React.useEffect(() => {
-      dispatch(currentUser(role));
-    }, []);
+  React.useEffect(() => {
+    dispatch(currentUser(role));
+  }, [role]);
 
-    return (
-        // <Route {...rest} render={props => (
-        //     isLoggedIn
-        //         ? <Component {...props} />
-        //         : <Redirect to={{ pathname: '/login' }} />
-        // )} />
-        <Route {...rest} render={props => (
-            isLoggedIn &&
-                <Component {...props} />
-        )} />
-    )
-}
+  return (
+    // <Route {...rest} render={props => (
+    //     isLoggedIn
+    //         ? <Component {...props} />
+    //         : <Redirect to={{ pathname: '/login' }} />
+    // )} />
+    <Route
+      {...rest}
+      render={(props) => isLoggedIn && <Component {...props} />}
+    />
+  );
+};
 
-export default PrivateRoute
+export default PrivateRoute;
