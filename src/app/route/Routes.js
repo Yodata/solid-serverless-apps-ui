8cc20966194950f6383ae453e8fece9a6b2c7721
adam-venord <adamvenord@hsfranchise.com>
2025-12-29 17:43:31 -0600

    UI changes for runAs=self

--- src/app/route/Routes.js
+++ src/app/route/Routes.js
@@ -6,22 +6,14 @@ import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
 import PrivateRoute from '../../components/Authentication/PrivateRoute';
 import { useSelector, useDispatch } from 'react-redux'
 import { setRoleName } from "../../redux/slices/roleSlice";
-import { getQueryParam, captureInitialQueryParams } from '../../utility';
 
 function Routes() {
   const state = useSelector(state => ({ id: state.auth.userId, userList: state.auth.userList }))
   const dispatch = useDispatch();
   let location = useLocation();
   useEffect(() => {
-    // Try to capture params again in case they appeared after initial load
-    captureInitialQueryParams();
-    // Use stored query parameter instead of location.search to handle Salesforce iframe URL rewriting
-    // Only set role if parameter is available, otherwise leave it undefined
-    const runAs = getQueryParam('runAs', location.search);
-    if (runAs) {
-      dispatch(setRoleName(runAs));
-    }
-  }, [dispatch, location.search]);
+    dispatch(setRoleName(location.search?.split("=")[1]));
+  }, []);
   return (
     <React.Fragment>
       <Switch>
@@ -30,7 +22,6 @@ function Routes() {
         {state.userList.some(ele => ele.contactId === state.id) &&
           (<PrivateRoute exact path="/admin" component={Admin} />)
         }
-        {/* Add new routes above this line */}
         <Redirect from="*" to="/" />
       </Switch>
     </React.Fragment>
--- src/components/Authentication/PrivateRoute.js
+++ src/components/Authentication/PrivateRoute.js
@@ -2,23 +2,16 @@ import React from 'react';
 import { Route, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { currentUser } from '../../redux/actions/authenticationActions';
-import { getQueryParam, captureInitialQueryParams } from '../../utility';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
     const dispatch = useDispatch();
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
     let location = useLocation();
-    
+    const role = location.search?.split("=")[1]
+
     React.useEffect(() => {
-      // Try to capture params again in case they appeared after initial load
-      captureInitialQueryParams();
-      // Use stored query parameter instead of location.search to handle Salesforce iframe URL rewriting
-      // Only set role if parameter is available, otherwise leave it undefined
-      const role = getQueryParam('runAs', location.search);
-      if (role) {
-        dispatch(currentUser(role));
-      }
-    }, [dispatch, location.search]);
+      dispatch(currentUser(role));
+    }, []);
 
     return (
         // <Route {...rest} render={props => (
