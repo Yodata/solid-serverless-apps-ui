29898c800108a2fc89bf3f6a8285d0b53ae71e8b
adam-venord <adamvenord@hsfranchise.com>
2025-12-29 14:34:23 -0600

    commented unnecessary code

--- src/components/Authentication/PrivateRoute.js
+++ src/components/Authentication/PrivateRoute.js
@@ -6,21 +6,21 @@ import { currentUser } from "../../redux/actions/authenticationActions";
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  let location = useLocation();
-  console.log(
-    "PrivateRoute rendering - isLoggedIn:",
-    isLoggedIn,
-    "location",
-    location
-  );
-  let role = null;
-  if (location.search?.includes("/self")) role = "self";
-  else if (location.search?.includes("/team")) role = "team";
-  console.log("PrivateRoute role from URL:", role);
+//   let location = useLocation();
+//   console.log(
+//     "PrivateRoute rendering - isLoggedIn:",
+//     isLoggedIn,
+//     "location",
+//     location
+//   );
+//   let role = null;
+//   if (location.search?.includes("/self")) role = "self";
+//   else if (location.search?.includes("/team")) role = "team";
+//   console.log("PrivateRoute role from URL:", role);
 
-  React.useEffect(() => {
-    dispatch(currentUser(role));
-  }, [role]);
+//   React.useEffect(() => {
+//     dispatch(currentUser(role));
+//   }, [role]);
 
   return (
     // <Route {...rest} render={props => (
