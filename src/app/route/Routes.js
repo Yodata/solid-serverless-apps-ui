1496904d6c7a871b0504c51d6ecb2b65862eea31
adam-venord <adamvenord@hsfranchise.com>
2025-12-29 13:34:33 -0600

    chnages for runAs default behaviour

--- src/app/route/Routes.js
+++ src/app/route/Routes.js
@@ -6,35 +6,14 @@ import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
 import PrivateRoute from '../../components/Authentication/PrivateRoute';
 import { useSelector, useDispatch } from 'react-redux'
 import { setRoleName } from "../../redux/slices/roleSlice";
-import { currentUser } from "../../redux/actions/authenticationActions";
 
 function Routes() {
   const state = useSelector(state => ({ id: state.auth.userId, userList: state.auth.userList }))
   const dispatch = useDispatch();
   let location = useLocation();
   useEffect(() => {
-    let role = null;
-    try {
-        const referrerUrl = new URL(document.referrer);
-        const referrerParams = new URLSearchParams(referrerUrl.search);
-        role = referrerParams.get('runAs');
-    } catch (e) {
-        // Fallback
-        role = null;
-    }
-    dispatch(currentUser(role));
-  }, [dispatch]);
-
-  // PostMessage listener for role from Salesforce parent
-  useEffect(() => {
-    const handler = (event) => {
-      if (event.origin.includes("bhhsresource.com")) {
-        dispatch(setRoleName(event.data.runAs));
-      }
-    };
-    window.addEventListener("message", handler);
-    return () => window.removeEventListener("message", handler);
-  }, [dispatch]);
+    dispatch(setRoleName(location.search?.split("=")[1]));
+  }, []);
   return (
     <React.Fragment>
       <Switch>
--- src/components/Authentication/PrivateRoute.js
+++ src/components/Authentication/PrivateRoute.js
@@ -4,9 +4,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import { currentUser } from '../../redux/actions/authenticationActions';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
+    const dispatch = useDispatch();
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+    let location = useLocation();
+    console.log("location in PrivateRoute:", location);
+    let role = "";
+    if(location.search?.split("=")[1].includes("self")) {;
+        role = "self";
+    } else if(location.search?.split("=")[1].includes("team")) {
+        role = "team";
+    } else {
+         role =location.search?.split("=")[1]
+    }
+
+    React.useEffect(() => {
+      dispatch(currentUser(role));
+    }, []);
 
     return (
+        // <Route {...rest} render={props => (
+        //     isLoggedIn
+        //         ? <Component {...props} />
+        //         : <Redirect to={{ pathname: '/login' }} />
+        // )} />
         <Route {...rest} render={props => (
             isLoggedIn &&
                 <Component {...props} />
