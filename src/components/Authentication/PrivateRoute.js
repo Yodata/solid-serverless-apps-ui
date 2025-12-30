7c8ef0fbbcbce36b884fde8f459ece9a487a6526
adam-venord <adamvenord@hsfranchise.com>
2025-12-30 10:14:43 -0600

    useSearchParams to read params

--- src/components/Authentication/PrivateRoute.js
+++ src/components/Authentication/PrivateRoute.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, useLocation, useSearchParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { currentUser } from '../../redux/actions/authenticationActions';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
     const dispatch = useDispatch();
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+    const [searchParams] = useSearchParams();
+     const runAs = searchParams.get("runAs");
+     console.log("PrivateRoute runAs", runAs);
     let location = useLocation();
     const role = location.search?.split("=")[1]
     console.log("PrivateRoute location", location);
