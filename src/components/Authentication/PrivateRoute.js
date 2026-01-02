4e54a86fe89fd2968b865f479b896a89107eeeb2
adam-venord <adamvenord@hsfranchise.com>
2026-01-02 13:26:52 -0600

    added the session for role

--- src/components/Authentication/PrivateRoute.js
+++ src/components/Authentication/PrivateRoute.js
@@ -16,11 +16,6 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 
   const param = sessionStorage.getItem("role");
   console.log("PrivateRoute sessionStorage role param:", param);
-  React.useEffect(() => {
-    setTimeout(() => {
-      sessionStorage.removeItem("role");
-    }, 3000);
-  }, []);
   return (
     // <Route {...rest} render={props => (
     //     isLoggedIn
